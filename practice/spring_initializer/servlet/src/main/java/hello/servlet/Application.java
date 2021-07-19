bb7e5273fe73ba0519b594d264386e6482a7eb0e
jeon3029 <jeon3029@naver.com>
2021-07-20 00:26:31 +0900

    save changes(servelet testing)

--- practice/spring_initializer/servlet/src/main/java/hello/servlet/Application.java
+++ practice/spring_initializer/servlet/src/main/java/hello/servlet/Application.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import hello.servlet.TestDto;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
