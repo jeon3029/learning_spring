c4de05023bbabead6fd3336814b8a551f2c8191f
jeon3029 <jeon3029@naver.com>
2021-07-19 22:45:48 +0900

    lombok test

--- practice/spring_initializer/servlet/src/main/java/hello/servlet/Application.java
+++ practice/spring_initializer/servlet/src/main/java/hello/servlet/Application.java
@@ -2,12 +2,10 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import hello.servlet.TestDto;
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
---
+++ practice/spring_initializer/servlet/src/main/java/hello/servlet/TestDto.java
@@ -0,0 +1,13 @@
+package hello.servlet;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+//lombok test 용도 클래스
+@Getter
+@Setter
+public class TestDto {
+    int testNumber;
+    String testString;
+}
