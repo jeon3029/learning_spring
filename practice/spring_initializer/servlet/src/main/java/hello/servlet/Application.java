5ec447d510460e7d59f5373e0cda0f3581555dd4
jeon3029 <jeon3029@naver.com>
2021-10-16 15:50:05 +0900

    add source on viewresolver

--- practice/spring_initializer/servlet/src/main/java/hello/servlet/Application.java
+++ practice/spring_initializer/servlet/src/main/java/hello/servlet/Application.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import hello.servlet.TestDto;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan 
 // 스프링 부트는 서블릿을 직접 등록해서 사용할 수 있도록 @ServletComponentScan 을 지원
@@ -12,4 +16,9 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+//	@Bean
+//	ViewResolver internalResourceViewResolver(){
+//		return new InternalResourceViewResolver("/WEB-INF/views/",".jsp");
+//	}
 }
