10c52b7f2d2c8fd03f923135c056ba5ed6016a25
jeon3029 <jeon3029@naver.com>
2022-01-02 14:09:24 +0900

    개발진행 : 상품목록

--- practice/spring_initializer/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ practice/spring_initializer/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -1,10 +1,16 @@
 package hello.itemservice.web.basic;
 
+import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Controller
 @RequestMapping("/basic/items")
@@ -13,10 +19,22 @@ public class BasicItemController {
     private final ItemRepository itemRepository;
 
     //required args constructor 으로 대체
-//    @Autowired
+//    @Autowired // constructor 가 하나일 떄 생략 가능
+        // di 관련 https://leejisoo860911.tistory.com/2 참고
 //    public BasicItemController(ItemRepository itemRepository) {
 //        this.itemRepository = itemRepository;
 //    }
 
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items",items);
+        return "basic/item";
+    }
 
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itema",20000,1));
+        itemRepository.save(new Item("itemb",20000,2));
+    }
 }
