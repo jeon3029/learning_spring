dc8a522527bda27e0f565a23bf3ff27e151865fc
taejoon.jeon <taejoon.jeon@navercorp.com>
2022-12-24 13:01:38 +0900

    remember me

--- spring_security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ spring_security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,10 +1,15 @@
 package io.security.basicsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
@@ -18,14 +23,40 @@ import java.io.IOException;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  UserDetailsService userDetailsService;
+
   @Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.authorizeRequests()
         .anyRequest().authenticated();
 		http
-        .formLogin();
-    http.logout()
+        .formLogin()
+        // .loginPage("/login")
+				.defaultSuccessUrl("/")
+				.failureUrl("/login")
+				.usernameParameter("userId")
+				.passwordParameter("passwd")
+				.loginProcessingUrl("/login_proc")
+				.successHandler(new AuthenticationSuccessHandler() {
+					@Override
+					public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+						System.out.println("success : " + authentication.toString());
+            httpServletResponse.sendRedirect("/");
+					}
+				})
+				.failureHandler(new AuthenticationFailureHandler() {
+					@Override
+					public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+						System.out.println("failed : " + e.getMessage());
+            httpServletResponse.sendRedirect("/login");
+					}
+				})
+        .permitAll() //loginPage 인증 해제
+    .and()
+      .logout()
         .logoutUrl("/logout")
         .logoutSuccessUrl("login")
         .deleteCookies("JSESSIONID","remember-me")
@@ -45,8 +76,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
               Authentication authentication) throws IOException, ServletException {
                 response.sendRedirect("/login");
           }
-        });
-				
+        })
+    .and()
+      .rememberMe()
+      .rememberMeParameter("remember")
+      .tokenValiditySeconds(3600)
+      .alwaysRemember(false)
+      .userDetailsService(userDetailsService)
+      ;
 
 	}
 }
